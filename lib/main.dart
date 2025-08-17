3340bc4cb25db48799d8de9300f04119dacfa0f9
diochecaleen <mechelleannsalvo@gmail.com>
2025-08-18 07:49:05 +0800

    feat: add person

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'computer.dart';
+import 'person.dart';
 
 void main() {
   var myComputer = Computer('Lenovo', 2021);
   myComputer.describe();
+
+  var person = Person('Dyleen', 18);
+  person.sayHello();
 }
---
+++ lib/person.dart
@@ -0,0 +1,10 @@
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+
+  void sayHello() {
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
